dee8f0f28890e161f82d577c5065757c8a854fa8
Raul Mainardi Neto <rmainardineto@gmail.com>
2020-02-14 18:00:50 -0300

    Adding frontend

--- youtube-search-api/handler.js
+++ youtube-search-api/handler.js
@@ -22,12 +22,20 @@ module.exports.search = async (event, context) => {
   }).then(result => {
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify(result)
     }
   }).catch(err => {
     return {
       statusCode: err.statusCode || 500,
-      headers: { 'Content-Type': 'text/plain' },
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Content-Type': 'text/plain' 
+      },
       body: 'Could not fetch from YouTube.'
     }
   })
