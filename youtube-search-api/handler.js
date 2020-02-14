c6c795554690dafa7c432f5c70aa835230429dd4
Raul Mainardi Neto <rmainardineto@gmail.com>
2020-02-14 15:26:37 -0300

    Initial commit

---
+++ youtube-search-api/handler.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const connectToDatabase = require('./db')
+const ytsearch = require('./youtube')
+const Entry = require('./models/entry')
+
+module.exports.search = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase().then(() => {
+    return event.queryStringParameters && event.queryStringParameters.q  ? ytsearch(event.queryStringParameters.q).then(
+      (result, err) => {
+        if (err) {
+          return Promise.reject(err)
+        } else {
+          return Entry.find({ '_id': { $in: result.map(value => value.id) } }).then(values => {
+            let ids = values.map(value => value.id)
+            let entries = result.filter(item => !ids.includes(item.id))
+            return Entry.create(entries).then(() => Promise.resolve(result))
+          })
+        }
+      }).catch(err => Promise.reject(err)): Promise.reject("Query is missing")
+  }).then(result => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    }
+  }).catch(err => {
+    return {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Could not fetch from YouTube.'
+    }
+  })
+}
